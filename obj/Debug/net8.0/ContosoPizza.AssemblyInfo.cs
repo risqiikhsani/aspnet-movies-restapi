9d544c38cd5ad617c511ce932bccdfc1645598c0
risqiikhsani <risqiikhsani16@gmail.com>
2024-10-09 20:58:15 +0700

    tst

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70d691cd85a32c9294f553f21873f8dc81177af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
